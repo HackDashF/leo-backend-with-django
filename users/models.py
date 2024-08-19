f985c15dbc2fd8911abc8130044095af41715d53
Moses Cragle <smcragle@gmail.com>
2024-08-19 13:07:35 -0700

    alter user model to include and require unique email

--- users/models.py
+++ users/models.py
@@ -2,10 +2,4 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    # although we add nothing to the User class at this point,
-    # we are taking control of the User class from the auth module
-    # this enables us to modify the User class in the future, if needed,
-    # 
-    # without this placeholder, changing the User class in the future 
-    # breaks the migrations system
-    pass
+    email = models.EmailField(unique=True)
