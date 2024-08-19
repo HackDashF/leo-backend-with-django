8856d55682f60aeb44f5a45d2e9e3aa5a54afbd8
Moses Cragle <smcragle@gmail.com>
2024-08-19 14:04:46 -0700

    own user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    # although we add nothing to the User class at this point,
+    # we are taking control of the User class from the auth module
+    # this enables us to modify the User class in the future, if needed,
+    # 
+    # without this placeholder, changing the User class in the future 
+    # breaks the migrations system
+    pass
