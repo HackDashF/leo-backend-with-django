afd19c2a4a50a8e6e1bf4b70780c2c32b340d68b
Moses Cragle <smcragle@gmail.com>
2024-09-28 10:42:18 -0700

    setup whitenoise to serve static files

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
 
 favicon_view = RedirectView.as_view(url='/static/favicon.svg', permanent=True)
 
 urlpatterns = [
-    path('favicon.ico', favicon_view)
+    path('favicon.ico', favicon_view),
+    path('', TemplateView.as_view(template_name='core/index.html'))
 ]
--- leo/settings/common.py
+++ leo/settings/common.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 from environs import Env
 from datetime import timedelta
@@ -58,6 +59,7 @@ AUTH_USER_MODEL = 'users.User'
 MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -167,7 +169,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
